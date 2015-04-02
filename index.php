b069df8d65731ac5b391ed88e1583d77610c4a86
NOURTIER Antoine <a.nourtier59@gmail.com>
2015-04-02 17:22:47 +0200

    Add design

--- index.php
+++ index.php
@@ -2,26 +2,28 @@
 <html>
   <head>
     <title>restmovie</title>
-    <link rel="stylesheet" href="stylesheets/stylesheet.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:100">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheets/stylesheet.css">
   </head>
   <body>
     <header class="col-md-12 col-sm-12 col-xs-12">
-      <h1 class="title">Restmovie</h1>
+      <h1 class="title">Restmovie.</h1>
     </header>
 
-    <content>
-      <input class="typeahead" type="text" placeholder="Films">
-
-      <form class="form-horizontal">
-        <div class="form-group">
-          <label for="searchMovie" class="col-sm-3 control-label">Rechercher un film</label>
-          <div class="col-sm-8">
-            <input type="email" class="form-control" id="searchMovie" placeholder="Entre le nom de votre film">
-          </div>
+    <div class="content">
+      <div class="box">
+        <div class="container-1">
+          <input class="col-md-12 col-xs-12 typeahead" type="search" id="search" placeholder="Rechercher un film" />
         </div>
-      </form>
-    </content>
+
+        <div class="searchButton">
+          <button type="button" class="btn btn-default" aria-label="Left Align">
+            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+          </button>
+        </div>
+      </div>
+    </div>
 
     <script type="text/javascript" src="scripts/jquery.js"></script>
     <script type="text/javascript" src="scripts/typeahead.js"></script>
