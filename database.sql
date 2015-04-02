45707d0f2f40276c1d6fa5451bd6032762912683
Baptiste Lecocq <hello@tiste.io>
2015-04-02 14:34:09 +0200

    Update database

--- database.sql
+++ database.sql
@@ -3,11 +3,16 @@ CREATE DATABASE IF NOT EXISTS restmovie;
 USE restmovie;
 
 CREATE TABLE movies (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    title VARCHAR(255),
-    link VARCHAR(255),
-    created_at DATETIME DEFAULT NOW(),
-    updated_at DATETIME DEFAULT NOW()
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255),
+  link VARCHAR(255),
+  picture VARCHAR(255),
+  type VARCHAR(255),
+  actors VARCHAR(255),
+  directors VARCHAR(255),
+  released_at DATETIME DEFAULT NOW(),
+  created_at DATETIME DEFAULT NOW(),
+  updated_at DATETIME DEFAULT NOW()
 );
 
 INSERT INTO movies (title, link)
